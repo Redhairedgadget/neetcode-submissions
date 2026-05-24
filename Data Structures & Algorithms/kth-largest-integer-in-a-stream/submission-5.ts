0f0d8784188606dcf03262bebe716046dbc53e73
Redhairedgadget <32018908+Redhairedgadget@users.noreply.github.com>
2026-05-24 13:00:04 +0200

    Add: kth-largest-integer-in-a-stream - submission-5

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-5.ts
@@ -0,0 +1,84 @@
+class KthLargest {
+    /**
+     * @param {number} k
+     * @param {number[]} nums
+     */
+    k: number;
+    heap: number[];
+    largest: number[];
+
+    constructor(k: number, nums: number[]) {
+        this.k = k;
+        // generate heap
+        this.heap = [-Infinity];
+        this.largest = [];
+
+        for (let num of nums) {
+            this.percolate(num)
+        }
+    }
+
+    percolate(val: number): void {
+        this.heap.push(val);
+
+        // Percolate up
+        let i = this.heap.length - 1;
+
+        while (this.heap[i] < this.heap[Math.floor(i/2)]) {
+            [this.heap[i], this.heap[Math.floor(i/2)]] = [this.heap[Math.floor(i/2)], this.heap[i]];
+            i = Math.floor(i/2)
+        }
+
+        while (this.heap.length - 1 > this.k) {
+            this.pop();
+        }
+    }
+
+    pop(): void {
+        if (this.heap.length <= 2) {
+            this.heap.length = 1;
+            return;
+        }
+
+        // Move last element to root
+        this.heap[1] = this.heap.pop()!;
+
+        let i = 1;
+
+        while (true) {
+            let smallest = i;
+            let left = i * 2;
+            let right = i * 2 + 1;
+
+            if (
+                left < this.heap.length &&
+                this.heap[left] < this.heap[smallest]
+            ) {
+                smallest = left;
+            }
+
+            if (
+                right < this.heap.length &&
+                this.heap[right] < this.heap[smallest]
+            ) {
+                smallest = right;
+            }
+
+            if (smallest === i) break;
+
+            [this.heap[i], this.heap[smallest]] =
+                [this.heap[smallest], this.heap[i]];
+
+            i = smallest;
+        }
+    }
+
+    /**
+     * @param {number} val
+     * @return {number}
+     */
+    add(val: number): number {
+        this.percolate(val);
+        return this.heap[1]
+    }
+}
