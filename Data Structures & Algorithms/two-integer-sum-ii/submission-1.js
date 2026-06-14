e0cee554f387c738a958ed5d2ff0b7460891a185
Redhairedgadget <32018908+Redhairedgadget@users.noreply.github.com>
2026-06-14 08:18:27 +0200

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let [l, r] = [0, numbers.length -1];
+
+        while (l < r) {
+            let sum = numbers[l] + numbers[r];
+            console.log(sum, l, r)
+            if (sum === target) return [l + 1, r + 1];
+            else {
+                if (sum < target) l++;
+                else r--;
+            }
+        }
+
+        return [-1, -1];
+    }
+}
