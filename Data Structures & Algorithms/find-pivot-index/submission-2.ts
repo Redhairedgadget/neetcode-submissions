ae9e825359404eaa13b4459cc7d4defc9f2a6847
Redhairedgadget <32018908+Redhairedgadget@users.noreply.github.com>
2026-06-24 13:50:46 +0200

    Add: find-pivot-index - submission-2

---
+++ Data Structures & Algorithms/find-pivot-index/submission-2.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    pivotIndex(nums: number[]): number {
+        let pre = [0];
+        let suf = [0];
+
+        let curPreSum = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+            curPreSum += nums[i];
+            pre.push(curPreSum)
+        }
+
+        let curSufSum = 0;
+
+        for (let i = nums.length - 1; i > -1; i--) {
+            curSufSum += nums[i];
+            suf.push(curSufSum)
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+            if (pre[i] === suf[nums.length - i - 1]) return i;
+        }
+
+        return -1;
+    }
+}
